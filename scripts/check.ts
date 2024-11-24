321d3a4704754c7a5f65b4e6d2ef7b313f2e1090
ntnyq <ntnyq13@gmail.com>
2024-11-25 07:23:09 +0800

    chore: check package.json before commit

---
+++ scripts/check.ts
@@ -0,0 +1,32 @@
+/**
+ * incase commit vscode extension manifest properties accidentally
+ */
+
+import process from 'node:process'
+
+import { readExtensionManifest } from '../src'
+
+export async function checkPackageJson() {
+  try {
+    const manifest = await readExtensionManifest()
+    if (
+      manifest.contributes ||
+      manifest.publisher ||
+      manifest.activationEvents ||
+      manifest.capabilities ||
+      manifest.categories ||
+      manifest.extensionPack ||
+      manifest.extensionPack
+    ) {
+      console.log('\n❗️ Unexpected properties in package.json')
+      return process.exit(1)
+    }
+
+    console.log('\n✅ package.json is valid')
+  } catch {
+    console.log('\n❗️ Ops, something went wrong')
+    process.exit(1)
+  }
+}
+
+await checkPackageJson()
